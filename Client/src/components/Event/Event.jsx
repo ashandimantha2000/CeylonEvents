17eed2d00d4cba3af8d5cec5d84c3c0c00d7aebd
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-06-13 13:53:40 +0530

    map: events

--- Client/src/components/Event/Event.jsx
+++ Client/src/components/Event/Event.jsx
@@ -1,32 +1,46 @@
 import styles from "./Event.module.scss";
 import event1 from "../../assets/images/Event_Images/event1.png";
+import content from "./Events.json";
 import { CiCalendar } from "react-icons/ci";
 import { CiClock2 } from "react-icons/ci";
 import { IoLocationOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 function Event() {
+  const location = useLocation();
+  let headerText = 'Upcoming Events';
+  if (location.pathname === '/events') {
+    headerText = 'Related Events';
+  }
   return (
-    <div className={styles.card}>
-      <div>
-        <img src={event1} alt="" />
-      </div>
-      <div className={styles.cardspace}>
-        <div className={styles.card_data}>
-          <CiCalendar />
-          <h5>27/08/2024</h5>
-          <CiClock2 />
-          <h5>7:30 PM</h5>
-          <IoLocationOutline />
-          <h5>Colombo 7</h5>
-        </div>
-        <h3>Event Name</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur. Fringilla orci ac cras morbi
-          tortor. At sit amet ipsum habitasse. Vitae pulvinar viverra sapien
-          tempus luctus gravida condimentum ornare. Cum justo vulputate in quis
-          augue ac.
-        </p>
-        <button>View Details</button>
+    <div>
+      <h1 className="headerss">{headerText}</h1>
+      <div className={styles.all}>
+        {content.map((event) => (
+          <div key={event.id} className={styles.card}>
+            <div>
+              <div>
+                <img src={event.image} alt="" />
+              </div>
+              <div className={styles.cardspace}>
+                <div className={styles.card_data}>
+                  <CiCalendar />
+                  <h5>{event.date}</h5>
+                  <CiClock2 />
+                  <h5>{event.time}</h5>
+                  <IoLocationOutline />
+                  <h5>{event.location}</h5>
+                </div>
+                <h3>{event.name}</h3>
+                <p>{event.description}</p>
+                <a href={event.link}>
+                  <button>View Details</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
